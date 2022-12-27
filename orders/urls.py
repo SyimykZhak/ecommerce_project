39f3e5dd93a75c4402afb5a4affcec89e3f49b1c
SyimykZhak <zhakshybaevv@gmail.com>
2022-12-27 13:28:27 +0600

    Export orders to CSV files

--- orders/urls.py
+++ orders/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "orders"
 urlpatterns = [
     url(r'^create/$', views.order_create, name='order_create'),
+    url(r'^admin/orders/order/(?P<order_id>\d+)/$', views.admin_order_detail, name='admin_order_detail'),
 ]
